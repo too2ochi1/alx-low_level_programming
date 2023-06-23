36e10d4c4363d082146a10a92ed0247e0fb17b5e
Jason Herbert <herbertj289@gmail.com>
2023-06-22 18:44:06 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle shape using
+ * # to the terminal for a number of times
+ * @size: The number of time the shape /$ should be displayed
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i <  size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < size - (i + 1))
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
