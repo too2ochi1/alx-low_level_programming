4f461f98da7bb1393b00a974e0c77a59e8f3b507
Jason Herbert <herbertj289@gmail.com>
2023-06-22 18:15:33 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,20 @@
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int letter);
+int _isdigit(int num);
+int mul(int num1, int num2);
+void _putchar(char c);
 void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
+void print_number(int n);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
+void print_most_numbers(void);
+void more_numbers(void);
+void _putchar(char c);
+
+#endif /* MAIN_H */
