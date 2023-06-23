bcd8d207806462389d367730bb1fbe9af518db0c
Jason Herbert <herbertj289@gmail.com>
2023-06-22 18:01:32 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - Calls the print_numbers function
+  *
+  * Return: 0 (Always success)
+  */
+
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
