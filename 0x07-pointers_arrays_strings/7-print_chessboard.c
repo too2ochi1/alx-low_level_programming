f31bf739e7025ccd4961f8c44db966c75fd1f151
Jason Herbert <herbertj289@gmail.com>
2023-07-03 11:33:43 -0700

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * print_chessboard - prints the chessboard
- * @a: 2D array representing the chessboard
- */
-void print_chessboard(char (*a)[8])
-{
-	int i, j;
+void printChessboard() {
+    char board[8][8] = {
+        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+    };
 
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			printf("%c", a[i][j]);
-		}
-		printf("\n");
-	}
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+            printf("%c ", board[row][col]);
+        }
+        printf("\n");
+    }
+}
+
+// Example usage
+int main() {
+    printChessboard();
+    return 0;
 }
